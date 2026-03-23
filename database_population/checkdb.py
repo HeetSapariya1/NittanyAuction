27afd7953d3a25daf65eeac3bc55529ee685a467
JustinHg04 <jwh6412@psu.edu>
2026-03-22 22:44:18 -0400

    accomodates subclasses

--- database_population/checkdb.py
+++ database_population/checkdb.py
@@ -19,6 +19,40 @@ with open("dataset/Users.csv", "r", encoding="utf-8") as file:
             row
         )
 
+------ 
+
+with open("dataset/Bidders.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    next(reader)
+
+    for row in reader:
+        cur.execute(
+            "INSERT INTO Bidders (email, first_name, last_name, age, home_address_id, major) VALUES (?, ?, ?, ?, ?, ?)",
+            row
+        )
+
+with open("dataset/Sellers.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    next(reader)
+
+    for row in reader:
+        cur.execute(
+            "INSERT INTO Sellers (email, bank_routing_number, bank_account_number, balance) VALUES (?, ?, ?, ?)",
+            row
+        )
+
+
+with open("dataset/Helpdesk.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    next(reader)
+
+    for row in reader:
+        cur.execute(
+            "INSERT INTO Helpdesk (email, Position) VALUES (?, ?)",
+            row
+        )
+
+--------
 conn.commit()
 
 cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
